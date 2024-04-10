fa9a32aed8e1a2d4e938e4885526b13bea9bbaeb
lovgao <ykl49799@163.com>
2024-04-10 21:49:50 +0800

    布局：底部footer模块

--- src/components/front/Footer.vue
+++ src/components/front/Footer.vue
@@ -1,11 +1,26 @@
 <template>
-<h1>底部</h1>
+  <div class="footerBox">
+    <p>地址：天津商业大学</p>
+    <p>电话：021-7823049</p>
+    <p>邮箱：ykldebishe@126.com</p>
+    <p>本站资源来源于互联网</p>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
+  .footerBox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 12px;
+    padding: 20px;
+    background: #bbbbbb;
+    color: #fff;
 
+    p {
+      height: 20px;
+    }
+  }
 </style>
--- src/views/front/MainView.vue
+++ src/views/front/MainView.vue
@@ -20,4 +20,17 @@ import NavMenu from "@/components/front/NavMenu.vue";
 import Footer from "@/components/front/Footer.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.common-layout {
+  min-height: 100%;
+  // 容器为flex布局
+  display: flex;
+  // 方向为纵向，保持正常的从上到下文档流
+  flex-direction: column;
+
+  Footer {
+    // 设置footer的上外边距为auto
+    margin-top: auto;
+  }
+}
+</style>
